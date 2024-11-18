9869246bec4e8a7429d9508642e47f8a8db321b7
Juan Gabriel Tavárez López <juangabriel2960@gmail.com>
2024-11-18 14:52:14 -0400

    refactor: replace fs blogs with fetch

--- actions/blogs/index.ts
+++ actions/blogs/index.ts
@@ -1,39 +1,28 @@
 "use server";
-import { promises as fs } from "fs";
 import { notFound } from "next/navigation";
-import path from "path";
 import { Blog } from "../../interfaces/entities/Blog";
 
 export const getBlog = async (slug: string): Promise<Blog> => {
-  try {
-    const file = await fs.readFile(
-      process.cwd() + `/public/posts/${slug}.json`,
-      "utf8"
-    );
-    const blog: Blog = JSON.parse(file);
-    return blog;
-  } catch (error) {
-    console.error(error);
+  const blogs = await getAllBlogs();
+
+  const data = blogs.find((blog) => blog.slug === slug);
+  if (!data) {
     notFound();
   }
+
+  return data;
 };
 
 export const getAllBlogsPaths = async (): Promise<string[]> => {
-  const files = await fs.readdir(process.cwd() + `/public/posts`, "utf8");
-  const paths = files.map((file) => path.basename(file, ".json"));
+  const blogs = await getAllBlogs();
 
-  return paths;
+  return blogs.map((blog) => blog.slug);
 };
 
 export const getAllBlogs = async (): Promise<Blog[]> => {
-  const files = await fs.readdir(process.cwd() + `/public/posts`, "utf8");
-
-  const results = await Promise.allSettled(
-    files.map((file) => fs.readFile(process.cwd() + `/public/posts/${file}`, "utf8"))
+  const data: Blog[] = await fetch(`${process.env.CLIENT_URL}/posts.json`).then(
+    (res) => res.json()
   );
-  const validBlogs = results
-    .filter((result) => result.status === "fulfilled")
-    .map((result) => result.value);
 
-  return validBlogs.map((v) => JSON.parse(v));
+  return data;
 };
